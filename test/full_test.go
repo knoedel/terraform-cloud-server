f19324fa7e0d0e392d0701461a30d74a3e73a223
Michael Haase <darkstar@gernox.de>
2021-08-06 17:21:18 +0200

    test: add test with all optional input variables

---
+++ test/full_test.go
@@ -0,0 +1,19 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+)
+
+func TestCloudServerFull(t *testing.T) {
+	t.Parallel()
+
+	terraformOptions := &terraform.Options{
+		TerraformDir: "../examples/full/",
+		NoColor:      true,
+	}
+
+	defer terraform.Destroy(t, terraformOptions)
+	terraform.InitAndApply(t, terraformOptions)
+}
